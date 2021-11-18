41f4807898d6d5c6e92f0706d1e37bdbe22ccde0
Mora Ogunro <18519716+MoraOgunro@users.noreply.github.com>
2021-11-17 19:50:49 -0600

    sliding columns

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,78 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="full-screen">
+    <div class="full-screen-content">
+      <div class="top"></div>
+      <div class="middle">
+        <vertical-grids/>
+      </div>
+      <!-- Start of Foot Component !-->
+      <div class="bottom">
+      </div>
+      <!-- End of Foot Component !-->
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+  import VerticalGrids from './components/LayoutComponents/VerticalGrids.vue'
+export default {
+  components: {
+    VerticalGrids
+  }
+}
+</script>
 
+<style lang="sass">
+$tablet: 600px
+$medium-desktop: 768px
+$desktop: 992px
+$widescreen: 1200px
+body, html
+  margin: 0
+  padding: 0
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  height: 100vh
+/* Wrap page in this for full screen, must be used with full-screen-content class */
+.full-screen
+  display: flex
+  width: 100vw
+  height: 100%
+  overflow-x: hidden
+  overflow-y: hidden
+/* Wrap all full screen content in this class */
+.full-screen-content
+  flex: 1
+  display: flex
+  flex-direction: column
+  width: 100%
+/* Wrap full screem header content */
+.top
+  flex-grow: 0
+  flex-shrink: 1
+  height: 0rem
+  width: 100%
+  background-color: blue
+  min-height: 0px
+/* Wrap full screen middle content */
+.middle
+  flex: 1
+  min-height: 150px
+  width: 100%
+  background-color: rgb(50,50 ,50 )
+/* Wrap footer content in this to get bottom allignment */
+.bottom
+  align-self: flex-end
+  width: 100%
+  flex-shrink: 1
+  flex-grow: 0
+@media screen and (max-width: $tablet)
+  .full-screen
+    overflow-y: auto
+    scrollbar-color: black white
+    scrollbar-width: thin
 </style>
---
+++ src/components/LayoutComponents/VerticalGrids.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid">
+   <div class="grid-item" style="background-color: rgb(150, 150, 150)">
+      <div class="item-text">
+         <div class="item-header">Atreites</div>
+         <div class="item-subtite">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat doloribus eum, aperiam, dolore iusto distinctio deleniti perspiciatis molestiae nemo quisquam saepe nostrum ducimus quos ratione odit quis maxime accusantium, exercitationem labore amet natus! Voluptatibus mollitia pariatur aliquam, vitae ullam sequi!</div>
+      </div>
+   </div>
+   <div class="grid-item" style="background-color: rgb(150, 120, 150)">
+      <div class="item-text">
+         <div class="item-header">Fremen</div>
+         <div class="item-subtite">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat doloribus eum, aperiam, dolore iusto distinctio deleniti perspiciatis molestiae nemo quisquam saepe nostrum ducimus quos ratione odit quis maxime accusantium, exercitationem labore amet natus! Voluptatibus mollitia pariatur aliquam, vitae ullam sequi!</div>
+      </div>
+   </div>
+   <div class="grid-item" style="background-color: rgb(110, 150, 150)">
+      <div class="item-text">
+         <div class="item-header">Bene Gesserit</div>
+         <div class="item-subtite">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat doloribus eum, aperiam, dolore iusto distinctio deleniti perspiciatis molestiae nemo quisquam saepe nostrum ducimus quos ratione odit quis maxime accusantium, exercitationem labore amet natus! Voluptatibus mollitia pariatur aliquam, vitae ullam sequi!</div>
+      </div>
+   </div>
+   <div class="grid-item" style="background-color: rgb(150, 150, 110)">
+      <div class="item-text">
+         <div class="item-header">Harkonen</div>
+         <div class="item-subtite">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat doloribus eum, aperiam, dolore iusto distinctio deleniti perspiciatis molestiae nemo quisquam saepe nostrum ducimus quos ratione odit quis maxime accusantium, exercitationem labore amet natus! Voluptatibus mollitia pariatur aliquam, vitae ullam sequi!</div>
+      </div>
+   </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+.grid
+    height: 100%
+    display: flex
+    flex-direction: row
+.grid-item
+    display: flex
+    transition: all 0.3s ease-in-out
+    width: 25%
+    &:hover
+        width: 80%
+        .item-text
+            transition: all 0.5s ease-in-out
+            transition-delay: 0.3s
+.item-text
+    writing-mode: sideways-lr
+    align-self: center
+.item-header
+    font-size: 4rem
+</style>
