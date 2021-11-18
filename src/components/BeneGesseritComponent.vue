5108a54dc95e0fe8a45a5ee63849d002079694bb
Mora Ogunro <18519716+MoraOgunro@users.noreply.github.com>
2021-11-17 18:41:31 -0600

    Initial Commit

---
+++ src/components/BeneGesseritComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Bene Gesserit
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HarkonenComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Harkonen
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
