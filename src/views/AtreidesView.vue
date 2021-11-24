58c5776d9a50b1eeed4aa6e844ceddf4f49917ee
Mora Ogunro <18519716+MoraOgunro@users.noreply.github.com>
2021-11-24 15:28:44 -0600

    Atreides Page Done

--- src/views/AtreidesView.vue
+++ src/views/AtreidesView.vue
@@ -1,6 +1,8 @@
 <template>
+  <div class="bg-image"></div>
   <section class="hero is-fullheight">
-    <div class="hero-head top-left-title">ATREIDES</div>
+    
+    <div class="hero-head top-left-title">{{name}}</div>
 
     <div class="hero-body">
       <div class="columns">
@@ -30,16 +32,16 @@
       </div>
     </div>
 
-    <div class="hero-foot">ATREIDES</div>
+    <div class="hero-foot">{{name}}</div>
   </section>
 </template>
 
 <script>
 export default {
   setup (){
-    const url = '/home/mora/projects/dune2/src/views/DUKE.png'
+    var name = 'ATREIDES'
     return{
-      url
+      name
     }
   }
 };
@@ -48,12 +50,49 @@ export default {
 <style lang="sass">
 $font-color: white
 $bg-color: black
+$transparent: rgba(0, 0, 0, 0)
+html
+  background-color: black
+  overflow: hidden
+.bg-image
+  position: absolute
+  height: 100%
+  width: 100%
+  background-image: url('/src/assets/caladan.jpg')
+  background-size: cover
+  background-position: center
+  filter: blur(4px)
+.hero-head
+  display: flex
+  font-weight: 800
+  font-size: min(15vw,20vh)
+  justify-content: flex-start
+  font-style: italic
+.hero
+  background-color: rgba(0, 0, 0,0.7)
+  color: $font-color
+  position: relative
+  padding-left: 2rem
+  padding-right: 2rem
+  p
+    font-size: 1rem
+    font-weight: 200
+.hero-foot
+  display: flex
+  font-weight: 800
+  font-size: min(15vw,20vh)
+  color: $transparent
+  -webkit-text-stroke: 2px $font-color
+  justify-content: flex-end
+  font-style: italic
+.column
+  align-self: center
 .button
   font-weight: 800
   letter-spacing: 3px
   border-color: $font-color
   margin-top: 1rem
-  background-color: $bg-color
+  background-color: $transparent
   color: $font-color
   font-size: 1rem
   border-width: 2px
@@ -62,19 +101,6 @@ $bg-color: black
   background-color: $font-color
   color: $bg-color
   border-width: 0px
-.hero-head
-  display: flex
-  font-weight: 800
-  font-size: min(15vw,20vh)
-  justify-content: flex-start
-  font-style: italic
-.hero
-  color: $font-color
-  background-color: $bg-color
-  padding: 2rem
-p
-  font-size: 1rem
-  font-weight: 200
 .piccol
   display: flex
   justify-content: center
@@ -89,16 +115,8 @@ p
   background-size: cover
   background-position: center
   border: 2px solid $font-color
-
   max-width: 650px
-.hero-foot
-  display: flex
-  font-weight: 800
-  font-size: min(15vw,20vh)
-  color: $bg-color
-  -webkit-text-stroke: 2px $font-color
-  justify-content: flex-end
-  font-style: italic
+
 @media screen and (max-width: 769px)
   .hero-head, .hero-foot
     justify-content: center
